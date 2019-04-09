8f1bd8b31b9ff96f4558fb68751fcd5dacf5cc9c
Luc Everse <luc@wukl.net>
2019-04-09 17:08:42 +0200

    Fix missing spaces in help text

--- geike.js
+++ geike.js
@@ -27,8 +27,8 @@ const help = [
         'stop! ➡️ Laat Geike stoppen met zingen als ze in een channel zit',
         'SCHREEUW ➡️ Laat Geike luidkeels haar fantastische geluid horen',
         'help ➡️ Laat Geike uitleggen naar welke commandos ze allemaal luistert',
-        'waar ben je ➡️Geike vertelt op welke server ze draait',
-        'kun je dit ook {vaak | soms | zelden} spelen __YT URL__ ➡️Geike voegt een nieuw nummer aan haar bibliotheek toe'
+        'waar ben je ➡ ️Geike vertelt op welke server ze draait',
+        'kun je dit ook {vaak | soms | zelden} spelen __YT URL__ ➡️ Geike voegt een nieuw nummer aan haar bibliotheek toe'
 ];
 
 const configLocation = '/etc/geike/geike.conf';
