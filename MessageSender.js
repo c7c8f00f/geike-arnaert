384c3206790833312f837029a5540b46c5c0a00c
Luc Everse <luc@wukl.net>
2021-03-06 14:52:00 +0100

    Reply directly, do not use a role Discord(.js)? is buggy and the unique-role-code kept @mentioning other people

--- MessageSender.js
+++ MessageSender.js
@@ -1,14 +1,6 @@
 export default class MessageSender {
   reply(msg, reply) {
-    const identifyingRoles = msg.member.roles.cache.filter(role => role.mentionable && role.members.size === 1);
-
-    if (identifyingRoles.size) {
-      return msg.channel.send(
-        `<@&${identifyingRoles.random().id}>, ${reply.replace(/^[ ]*/, '')}`, { split: true }
-      );
-    }
-
-    // If the code comes to here, there is no unique role for the member, thus simply replying.
+    // Discord(.js)? is buggy and the unique-role-code kept @mentioning other people
     return msg.reply(reply);
   }
 };
